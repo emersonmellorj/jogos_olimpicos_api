9fba8ee855cfdb2283f8054eaa53531f91e45662
Emerson Mello <emersonmello@Emersons-MacBook-Air.local>
2020-04-25 11:20:28 -0300

    Agora a API esta validando a quantidade de resultados cadastrados para cada atleta em cada etapa da modalidade antes de inserir os dados no banco. Se a quantidade máxima for ultrapassada, será retornada uma mensagem de erro para o usuário.

--- jogos/views.py
+++ jogos/views.py
@@ -55,8 +55,29 @@ class ResultsViewSet(viewsets.ModelViewSet):
             if int(request.data["stage"]) == data.id:
                 relationship = True
 
-        # Check if stage in modality is finished
-        if stage_status and relationship:
+        """
+        Each athlete have 3 chances in "Lançamento de Dardo" for each stage
+        Each athlete have 1 chance in "100m Rasos" for each stage
+        """
+        chances_athlete = Results.objects.filter(
+            stage=stage, 
+            modality=request.data["modality"], 
+            athlete=request.data["athlete"]
+        ) 
+        len_results = len([chance.value for chance in chances_athlete])
+        modality_name = Modality.objects.get(pk=request.data["modality"]).name
+        have_chances = False
+
+        if len_results == 0:
+            have_chances = True
+        elif len_results < 3:
+            if '100m' in modality_name:
+                have_chances = False
+            elif len_results <= 2:
+                have_chances = True
+
+        # Validations before add result in DB
+        if stage_status and relationship and have_chances:
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
@@ -66,8 +87,13 @@ class ResultsViewSet(viewsets.ModelViewSet):
                 context = {
                     "mensagem": "Esta etapa não pertence a modalidade escolhida."
                 }
-            else:
+            elif stage_status == False:
                 context = {
                     "mensagem": "Esta etapa da competição já encontra-se encerrada."
                 }
+            else:
+                context = {
+                    "mensagem": "Para esta etapa e modalidade ja existe(m) resultado(s) cadastrado(s) para o atleta."
+                }
+            
             return Response(context, status=status.HTTP_403_FORBIDDEN)
